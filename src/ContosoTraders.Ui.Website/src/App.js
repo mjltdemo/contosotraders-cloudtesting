ce8797dd1ab916ed9f5a25e6248ba4d2513bf4a5
lambdapro <mudassars@lambdatest.com>
2023-07-26 23:42:33 +0530

    mon

--- src/ContosoTraders.Ui.Website/src/App.js
+++ src/ContosoTraders.Ui.Website/src/App.js
@@ -71,7 +71,7 @@ import { getCartQuantity } from "./actions/actions";
       <div className={`App ${props.theme ? 'dark' : 'light' }`}>
         <Fragment>
           <div className="mainHeader">
-            <HeaderMessage type="warning" icon={warningIcon} message="Monsoon Sale 2 is Live !"/>
+            <HeaderMessage type="warning" icon={warningIcon} message="Monsoon Sale is Live !"/>
             <Appbar />
             {location.pathname === '/' || location.pathname === '/new-arrivals' ?
               <Header/>
