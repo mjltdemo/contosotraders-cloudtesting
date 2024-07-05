0f12c5104f9330c1fe32e7665b8a0bc1523f5889
mjltdemo <mjltdemo@gmail.com>
2024-07-06 03:02:36 +0530

    Saturday Sale build

--- src/ContosoTraders.Ui.Website/tests/verifymsg.spec.ts
+++ src/ContosoTraders.Ui.Website/tests/verifymsg.spec.ts
@@ -4,15 +4,20 @@ test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:3000/');
 });
 
-
-
-
 test.describe('msg Validation', () => {
-   test('verify message on site', async ({ page }) => {
-      await page.getByText("Saturday Sale Week is Live !")
-      await expect(page).toBeTruthy();
-    });
-  });   
+  test('verify message on site', async ({ page }) => {
+    await page.getByText("Saturday Sale Week is Live !")
+     await expect(page).toBeTruthy();
+   });
+ })
+
+
+//test.describe('msg Validation', () => {
+//   test('verify message on site', async ({ page }) => {
+//     await page.getByText("Saturday Sale Week is Live !")
+//      await expect(page).toBeTruthy();
+//    });
+//  });   
 
  // test('verify message on site', async ({ page }) => {
  //   await page.getByText("Memorial Sale Week is Live !")
